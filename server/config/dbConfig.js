55436d75b797fc5092b95dc5b8955a9ec36b69be
madhusudan <madhusudanrathore722@gmail.com>
2025-02-23 07:39:49 +0530

    little

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -8,7 +8,7 @@ import mysql from 'mysql2/promise'
 // })
 const connection=mysql.createPool({
     user:'mansarawa',
-    host:'mydatabase-xyz.render.com',
+    host:'mydatabase-xyz..internal',
     database:'twitter',
     password:'mansa@7773',
     port:3306
