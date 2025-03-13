fdf265233e9dba00dd94765975d3090e764757a9
madhusudan <madhusudanrathore722@gmail.com>
2025-03-13 06:55:33 +0530

    db updated

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,18 +1,18 @@
 import mysql from 'mysql2/promise'
 
-const connection=mysql.createPool({
-    user:'root',
-    host:'localhost',
-    database:'twitter',
-    password:''
-})
 // const connection=mysql.createPool({
-//     user:process.env.DB_USER,
-//     host:process.env.DB_HOST,
-//     database:process.env.DB_NAME,
-//     password:process.env.DB_PASSWORD,
-//     port:process.env.DB_PORT
+//     user:'root',
+//     host:'localhost',
+//     database:'twitter',
+//     password:''
 // })
+const connection=mysql.createPool({
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database:process.env.DB_NAME,
+    password:process.env.DB_PASSWORD,
+    port:process.env.DB_PORT
+})
 
 connection.getConnection((err,connection)=>{
     if(err){
